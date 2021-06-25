1c4248af7c5af33ea8cf0527af211549865fcf2f
Kat Duff <katrina348@gmail.com>
2021-06-25 17:06:28 -0600

    data normalized

--- client/src/pages/Available.js
+++ client/src/pages/Available.js
@@ -1,5 +1,19 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import SellerProducts from '../components/SellerProducts';
+
+// {"first_name":"Arlene",
+// "last_name":"Grimes",
+// "sold":false,
+// "email":"chong@streich.name",
+// "seller_id":1,
+// "name":"Elderberry",
+// "price":3.0,
+// "category":"Miniatures",
+// "product_id":1,
+// "city":"Port Raleigh",
+// "zip":"92426",
+// "id":null},
 
 const seller = {
   fullName: 'Selly McSellerton',
@@ -31,6 +45,44 @@ const seller1 = {
 const sellers = [seller, seller1]
 
 const Available = () => {
+  let [sellers, setSellers] = useState([])
+  useEffect(()=>{
+    getSellerProducts()
+  },[])
+
+    const normalizeData = (sellerProducts) => {
+      //get unique ids of sellers
+      const uniqSellerIDS = [...new Set(sellerProducts.map(p=> p.seller_id))]
+      const sellers = []
+      uniqSellerIDS.forEach(id => {
+        let products = sellerProducts.filter(sp => sp.seller_id === id)
+        let {seller_id, first_name, last_name, email} = products[0]
+
+        const cleanedProducts = products.map(p => {
+          return{ name: p.name,
+                  price: p.price,
+                  category: p.category,
+                  city: p.city,
+                  zip: p.zip
+                }
+        })
+
+        sellers.push({
+         seller_id, fullName: `${first_name} ${last_name}`,
+         email, 
+         products: cleanedProducts
+        })
+      })
+      console.log(sellers)
+    }
+    const getSellerProducts = async()=>{
+      try {
+        let res = await axios.get('/api/products')
+        normalizeData(res.data)
+      }catch (err) {
+        console.log(err)
+      }
+  }
   return (
     <>
     <h1>Available</h1>
